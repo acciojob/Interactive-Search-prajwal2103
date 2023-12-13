d14317acb4fe44e1a0a4f8968b2e21729f9f6730
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-13 15:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let search=document.querySelector(".search");
 let button=document.querySelector(".btn");
 let input=document.querySelector(".input")
 button.addEventListener("click",()=>{
-	search.active;
+	// search.active;
 	input.focus();
 })
