01d76385bc59772f7cc2ce967f1f8f9a922381ad
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-04-05 22:21:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let search=document.querySelector(".search-bar");
+
 let button=document.querySelector(".btn");
 let input=document.querySelector(".input")
 button.addEventListener("click",()=>{
-	 search.active;
+	input.style.outline="auto";
 	input.focus();
 })
